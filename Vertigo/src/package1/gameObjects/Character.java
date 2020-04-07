3ed04658baadf2178a85d3ced79a191a3bc9a4f5
Thomas John Przybylski <przybylski.thomas.j@gmail.com>
2020-04-07 16:07:35 -0400

    Added some functionality to character

--- Vertigo/src/package1/gameObjects/Character.java
+++ Vertigo/src/package1/gameObjects/Character.java
@@ -15,52 +15,70 @@ public class Character implements GameObject {
 	int [] position;
 	int [] hitbox;
 	int [] size;
+	int currentState;
+	//possible version: 0 = idle, 1 = walkingA, 2 = walkingB, 3 = runningA, 4 = runningB, 5 = jumping
+	//alt 1: idle = 0, walking = 1 or -1, 
 	
 	
 	public Character() {
-		// TODO Auto-generated constructor stub
+		// Constructor for default character
 	}
 
 	@Override
 	public int[] getPosition() {
-		// TODO Auto-generated method stub
+		//get method for the position of the character 
+		//to be mesured from the ____(top left or bottom left?)
 		return position;
 	}
 
 	@Override
 	public void setPosition(int[] newPosition) {
-		// TODO Auto-generated method stub
+		// Set method for the character position
 		this.position = newPosition;
 	}
 
 	@Override
-	public BufferedImage getVisual(int current) {
-		// TODO Auto-generated method stub
-		return visual [current];
+	public BufferedImage getVisual(int currentState) {
+		// Geting one visual based on character state.
+		return visual [currentState];
 	}
 
 	@Override
 	public int[] getHitbox() {
-		// TODO Auto-generated method stub
+		// get method for the hitbox of the character
+		// 2 ints distance from position start point
+		// 2 ints hit box size, starting from the point given
+		// ex {1, 2, 3, 4} gets a hit box in one up two that is 3x4 in size
 		return hitbox;
 	}
 
 	@Override
 	public void setHitbox(int[] newHitbox) {
-		// TODO Auto-generated method stub
+		//set method for the hitbox
 		this.hitbox = newHitbox;
 	}
 	
 	public int [] getSize()
 	{
-		
+		//getter for the size of the character from (top left?) to (bottom right?) of character
 		return size;
 	}
 		
 	public void setSize(int [] newSize)
 	{
+		//setter for the character size
 		this.size = newSize;
 		
+	}
+	public int getCurrentState()
+	{
+		//gets the state of the character for images (Running, Jumping, Idle, etc) 
+		return this.currentState
+	}
+	public void setCurrentState(int newState)
+	{
+		//sets the curent state of the Character 
+		this.currentState = newState;
 	}
 
 }
